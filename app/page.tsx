9a7cbb4e62ae56990c4e47112e40db320a9421ab
xu <hieudd0899@gmail.com>
2023-10-29 19:40:10 +0700

    Create code base

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-export default function Home() {
-    return (
-        <main>
-            <Button>Home page</Button>
-        </main>
-    );
-}
